4e5e2656174f19de2d2d596b1f797258750c5236
Stefan Todorov <stefan.b.todorov@gmail.com>
2023-07-13 04:31:30 -0600

    remove everything and start over because why not

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ['./src/**/*.{ts,tsx,mdx}'],
-    darkMode: 'class',
     plugins: [require('@tailwindcss/typography')],
 };
