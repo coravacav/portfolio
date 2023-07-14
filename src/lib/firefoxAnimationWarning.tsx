82a398a609a446786e76feb01a0751ee1eea7799
Stefan Todorov <stefan.b.todorov@gmail.com>
2023-07-14 10:28:24 -0600

    update firefox warning

--- src/lib/firefoxAnimationWarning.tsx
+++ src/lib/firefoxAnimationWarning.tsx
@@ -6,7 +6,10 @@ export default function FirefoxAnimationWarning() {
     if (!isFirefox) return null;
 
     return (
-        <div className="fixed bottom-0 right-0 p-2 rounded text-xs" title="It's not the website having a problem.">
+        <div
+            className="fixed bottom-0 right-0 p-2 rounded-t rounded-l text-xs bg-gray-900 text-gray-300"
+            title="It's not the website having a problem."
+        >
             If you notice stuttering, Firefox has trouble rendering animations
         </div>
     );
