0c30373bc9b1031e092a8e8f26f5d3d18e6c0507
Stefan Todorov <stefan.b.todorov@gmail.com>
2023-07-16 03:35:42 -0600

    style tweaks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
                         'absolute w-full mt-4 md:mt-3 left-0 transition-all duration-[4s] ease-out gap-x-4',
                         helixStyles
                     )}
-                    strokeColor="stroke-hover"
+                    strokeColor="stroke-neutral-950"
                     height={100}
                     style="none"
                     direction="right"
