edd0e13ffeb84fb3d84391003a41cf45a580cd03
Stefan Todorov <stefan.b.todorov@gmail.com>
2023-07-16 03:24:40 -0600

    I like neutral more

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
     theme: {
         colors: {
             ...colors,
-            neutral: colors.gray,
+            neutral: colors.neutral,
             active: customColors.tropicalIndigo,
             hover: customColors.tangerine,
             activatable: customColors.pink,
