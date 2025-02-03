ee8fee07e45a605aa3ab246ca0089d57576a4c45
Stefan Todorov <stefan.b.todorov@gmail.com>
2025-02-02 21:22:56 -0600

    don't use a tar

---
+++ rjplc-wasm/rjplc_wasm_bg.wasm.d.ts
@@ -0,0 +1,13 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_output_free: (a: number, b: number) => void;
+export const output_lex_output: (a: number) => [number, number];
+export const output_parse_output: (a: number) => [number, number];
+export const output_type_check_output: (a: number) => [number, number];
+export const perform_steps: (a: number, b: number) => number;
+export const __wbindgen_export_0: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_start: () => void;
